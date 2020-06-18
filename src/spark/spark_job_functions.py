3dd570f577bdc87051ce0d7538a7e462fee4ccd1
Meghal Gandhi <meghalgandhi@Meghals-MBP.lan1>
2020-06-18 09:37:58 -0700

    Update and optimized spark jobs

--- src/spark/spark_job_functions.py
+++ src/spark/spark_job_functions.py
@@ -2,6 +2,8 @@ from pyspark.sql import SQLContext
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StringType
 from pyspark.sql.types import DoubleType
+from pyspark.sql import functions
+from pyspark.sql.functions import col, expr, when
 
 class SparkJobFunctions(object):
 
@@ -13,29 +15,58 @@ class SparkJobFunctions(object):
 		self.__session = SparkSession.builder \
 			.master('spark://ec2-52-13-56-58.us-west-2.compute.amazonaws.com:7077') \
 			.appName('GDELT-Business-Influencers') \
-			.config('spark.executors.memory', '12gb') \
 			.config('spark.jars', '/usr/local/spark/jars/postgresql-42.2.13.jar') \
-			.config('spark.executor.instances', 11) \
+			.config("spark.hadoop.mapreduce.fileoutputcommitter.algorithm.version", "2") \
 			.getOrCreate()
+		#.config('spark.executor.instances', 11) \
+		# .config('spark.executors.memory', '12gb') \
 
 		self.sqlcontext = SQLContext(self.__session)
 		# self.bucket1_gdelt = 's3n://gdelt-open-data/events/[1,2][0-9]*[0-9][0-3|9].csv'
 		# self.bucket2_gdelt = 's3n://gdelt-open-data/events/*.export.csv'
 
+		# self.bucket1_gdelt = 's3a://gdelt-open-data/events/1992.csv'
+		# self.bucket2_gdelt = 's3a://gdelt-open-data/events/20190415.export.csv'
+		# #self.bucket3_gdelt_mentions = 's3a://gdelt-open-data/v2/mentions/20190415[0-9][0-9][0-9][0-9][0-9][0-9].mentions.csv'
+		# self.bucket3_gdelt_mentions = 's3a://gdelt-open-data/v2/mentions/20190415*'
+
 		self.bucket1_gdelt = 's3a://gdelt-open-data/events/1992.csv'
-		self.bucket2_gdelt = 's3a://gdelt-open-data/events/20190415.export.csv'
-		#self.bucket3_gdelt_mentions = 's3a://gdelt-open-data/v2/mentions/20190415[0-9][0-9][0-9][0-9][0-9][0-9].mentions.csv'
-		self.bucket3_gdelt_mentions = 's3a://gdelt-open-data/v2/mentions/20190415*'
+		# self.bucket2_gdelt = 's3a://gdelt-open-data/events/201505*.export.csv'
+		# # self.bucket3_gdelt_mentions = 's3a://gdelt-open-data/v2/mentions/20190415[0-9][0-9][0-9][0-9][0-9][0-9].mentions.csv'
+		# self.bucket3_gdelt_mentions = 's3a://gdelt-open-data/v2/mentions/201505*.mentions.csv'
+
+		#self.bucket2_gdelt = 's3a://gdelt-open-data/events/2019*.export.csv'
+		# self.bucket3_gdelt_mentions = 's3a://gdelt-open-data/v2/mentions/20190415[0-9][0-9][0-9][0-9][0-9][0-9].mentions.csv'
+		self.bucket3_gdelt_mentions = 's3a://gdelt-open-data/v2/mentions/2019*.mentions.csv'
 
 	def sql_read(self, s3bucket, schema):
 		'''
 		Read GDELT data and return schema attached df
 		'''
-		gdelt_text_data = self.__session.sparkContext.textFile(s3bucket)
-		gdelt_with_schema = gdelt_text_data.map(lambda c: c.split("\t")).map(lambda c: schema(*c))
-		gdelt_df = self.sqlcontext.createDataFrame(gdelt_with_schema)
+		# gdelt_text_data = self.__session.sparkContext.textFile(s3bucket)
+		# gdelt_with_schema = gdelt_text_data.map(lambda c: c.split("\t")).map(lambda c: schema(*c))
+		# gdelt_df = self.sqlcontext.createDataFrame(gdelt_with_schema)
+		gdelt_df = self.sqlcontext.read \
+			.format('com.databricks.spark.csv') \
+			.option('header', 'false') \
+			.option('delimiter', "\t") \
+			.option("mode", "DROPMALFORMED") \
+			.schema(schema) \
+			.load(s3bucket)
+		#gdelt_df.show(n=10)
 		return gdelt_df
 
+	def sql_read_with_repartition(self,  s3bucket, schema, repartiton_col):
+		#gdelt_df = self.sqlcontext.read.csv(s3bucket, schema)
+		gdelt_df = self.sqlcontext.read \
+			    .format('com.databricks.spark.csv') \
+	            .option('header','false') \
+	            .option('delimiter',"\t") \
+				.option("mode", "DROPMALFORMED") \
+				.schema(schema) \
+	            .load(s3bucket).repartition(repartiton_col)
+		#gdelt_df.show(n=10)
+		return gdelt_df
 
 	def filter_gdelt_events(self, df):
 		'''
@@ -47,14 +78,15 @@ class SparkJobFunctions(object):
 		#roles_of_interest = ["COP", "GOV", "AGR", "BUS", "DEV", "EDU", "ENV", "HLH", "MED", "NGO"]
 
 
-		# Filter by US location only, Normalize GoldsteinScale and filter Actor1Type1Code #
-		events_df = self.sqlcontext.sql("select GLOBALEVENTID, SQLDATE, MonthYear, Year, Actor1Code, Actor1Type1Code, ActionGeo_FullName, "
+		# Filter by US location only, Normalize GoldsteinScale(min = -10, max = 10) and filter Actor1Type1Code #
+		events_df = self.sqlcontext.sql("select GLOBALEVENTID, SQLDATE, MonthYear, Year, Actor1Code, Actor1Type1Code, "
 										"ActionGeo_CountryCode, ActionGeo_ADM1Code, substring(ActionGeo_ADM1Code, 3,2) state,Actor1Geo_CountryCode, "
 										"GoldsteinScale, CAST((GoldsteinScale + 10) * 100 / 20 AS DOUBLE) GoldsteinScale_norm "
 										"from GdeltEventsTable where  ActionGeo_CountryCode='US' and "
 										"Actor1Code != 'null' and "
 										"Actor1Type1Code in ('COP', 'GOV', 'AGR', 'BUS', 'DEV', 'EDU', 'ENV', 'HLH', 'MED', 'NGO')" )
-		events_df.show(n=20)
+		# events_df.show(n=20)
+		events_df = events_df.filter(events_df.state != '')
 		return events_df
 
 	def filter_gdelt_mentions(self, df):
@@ -64,69 +96,68 @@ class SparkJobFunctions(object):
 		df.registerTempTable("GdeltMentionsTable")
 
 		# Filter to get required columns only #
-		mentions_df = self.sqlcontext.sql("select GLOBALEVENTID, avg(Confidence) as Avg_Confidence from GdeltMentionsTable group by GLOBALEVENTID")
-		mentions_df.show(n=20)
+		# mentions_df = self.sqlcontext.sql("select GLOBALEVENTID, avg(Confidence) as Avg_Confidence from GdeltMentionsTable group by GLOBALEVENTID")
+		mentions_df = self.sqlcontext.sql(
+			"select GLOBALEVENTID, MentionTimeDate, Confidence from GdeltMentionsTable")
+
+		#mentions_df.show(n=20)
 		return mentions_df
+
+	def write_s3_parquet_events(self, filtered_events_df):
+		# partiton by state, Year, Month, Day
+		new_filtered_events_df = filtered_events_df.withColumn('Month', expr('substring(SQLDATE, 5, 2)'))\
+													.withColumn('Day', expr('substring(SQLDATE, 7, 2)'))
+
+		# Write to parquet in S3
+		# cols = ['state', 'Year', 'Month', 'Day']
+		cols = ['Year', 'Month', 'Day']
+		new_filtered_events_df.repartition(*cols).write.mode('append').option("mode", "DROPMALFORMED").partitionBy(cols).parquet('s3a://gdelt-parquet/v2_events')
+		print ('..............................Written successfully on S3 parguet-events............................')
+		return
+
+	def write_s3_parquet_mentions(self, filtered_mentioned_df):
+		# partiton by Year, Month, Day, Hour, Min - Window
+		# cond = """case when expr('substring(MentionTimeDate, 11, 2)') == 00 then '1'
+        #     else case when expr('substring(MentionTimeDate, 11, 2)') == 15 then '2'
+        #         else case when expr('substring(MentionTimeDate, 11, 2)') == 30 then '3'
+        #             else case when expr('substring(MentionTimeDate, 11, 2)') == 45 > 0 then '4'
+        #                 end
+        #             end
+        #         end
+        #     end as Window"""
+
+		new_filtered_mentioned_df = filtered_mentioned_df.withColumn('Year',  expr('substring(MentionTimeDate, 1, 4)')) \
+													.withColumn('Month', expr('substring(MentionTimeDate, 5, 2)'))\
+													.withColumn('Day', expr('substring(MentionTimeDate, 7, 2)')) \
+													.withColumn('Hour', expr('substring(MentionTimeDate, 9, 2)')) \
+																.withColumn('Minute', expr('substring(MentionTimeDate, 11, 2)'))
+
+		# Write to parquet in S3
+		cols = ['Year', 'Month', 'Day', 'Hour', 'Minute']
+		new_filtered_mentioned_df.repartition(*cols).write.mode('append').option("mode", "DROPMALFORMED").partitionBy(cols).parquet('s3a://gdelt-parquet/mentions_data')
+		print ('..............................Written successfully on S3 parguet - mentions..........................')
+		return
 
 	def join_df(self, events_df, mentions_df):
 		'''
 		Joining of events data with mentions data
 		'''
 
-		final_df = events_df.join(mentions_df, on=['GLOBALEVENTID'], how='inner')
-		final_df.show(n=20)
-		return
+		#final_df = events_df.join(mentions_df, on=['GLOBALEVENTID'], how='inner')
+		#final_df = events_df.join(mentions_df, 'GLOBALEVENTID')
+		final_df = mentions_df.join(broadcast(events_df), 'GLOBALEVENTID')
+		#final_df.show(n=20)
+		return final_df
 
-	# def add_state(self, df):
-	# 	name = 'ActionGeo_ADM1Code'
-	# 	udf = UserDefinedFunction(lambda x: x[:2] + '-' + x[2:], StringType())
-	# 	df_news = df_news.select(*[udf(column).alias(name) if column == name
-	# 							   else column for column in df_news.columns])
-	# 	split_col = F.split(df_news['ActionGeo_ADM1Code'], '-')
-	# 	df_news = df_news.withColumn('action_state', split_col.getItem(1))
-	#
-	# 	return df_news
+	def aggregate_job(self, df_joined):
 
+		df_finalized = df_joined.groupby('state', 'Year', 'MonthYear', 'Actor1Type1Code') \
+			.agg(
+				functions.approx_count_distinct('GLOBALEVENTID').alias('events_count'),
+				functions.sum('GoldsteinScale_norm').alias('GoldsteinScale_norm_sum'),
+				functions.sum('Avg_Confidence').alias('confidence_sum')
+				)
 
-
-
-
-	# combine data frames - optional
-	# filter data frames by us only and for specific type of sector - done
-	# parse , add state column -
-	# normalize golstein and confidence score
-
-	# aggregated df and prepare it to write in database postgre sql
-
-	# def write_to_db(self, dataframe):
-	# 	__db_name = 'gdeltdatabase'
-	# 	__db_user = 'postgres'
-	# 	__db_pass = 'sai'
-	# 	__db_host = 'ec2-52-43-146-226.us-west-2.compute.amazonaws.com'
-	# 	__db_port = 5432
-	#
-	# 	__db_url = "jdbc:postgresql://" + __db_host + ':' + str(__db_port) + '/' + __db_name
-	#
-	# 	__table_name = 'gdelt'
-	#
-	# 	__properties = {
-	# 		"driver": "org.postgresql.Driver",
-	# 		"user": __db_user,
-	# 		"password": __db_pass
-	# 	}
-	# 	__write_mode = 'append'
-	# 	dataframe.write.jdbc(url=__db_url,
-    #           table=__table_name,
-	# 		  mode=__write_mode,
-    #           properties=__properties)
-	#
-	# 	print('Success.....Done')
-	#
-	# 	return
+		return df_finalized
 
 
-# gdelt_df = self.sqlcontext.read \
-#             .format('com.databricks.spark.csv') \
-#             .options(header='false') \
-#             .options(delimiter="\t") \
-#             .load(s3bucket, schema=schema)

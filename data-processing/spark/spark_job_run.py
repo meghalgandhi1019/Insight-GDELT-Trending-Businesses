b1d6823644f5e934d05e2655adae2efbde2df1be
Meghal Gandhi <meghalgandhi@Meghals-MBP.lan1>
2020-06-23 19:50:36 -0700

    Added combine df and comments

--- data-processing/spark/spark_job_run.py
+++ data-processing/spark/spark_job_run.py
@@ -7,17 +7,23 @@ if __name__ == "__main__":
     sparkjob_batch = SparkJobFunctions()
     dbfunction = DatabaseFunctions()
 
-    # 1. Read gdelt v2 events data and attach schema.
-    gdelt_df_events = sparkjob_batch.sql_read(sparkjob_batch.bucket_gdelt_old_events,
+    # 1. Read historical gdelt events data sets, attach schema and combine them.
+    gdelt_df_events_old = sparkjob_batch.sql_read(sparkjob_batch.bucket_gdelt_old_events,
                                          gdelt_schemas.schemaGdelt)
+    gdelt_df_events_new = sparkjob_batch.sql_read(sparkjob_batch.bucket_events_2019_onwards,
+                                         gdelt_schemas.schemaGdelt)
+    gdelt_combined_events_df = sparkjob_batch.combine_dfs(gdelt_df_events_old, gdelt_df_events_new)
 
-    # 2. Read gdelt mentions data with repartition and attach schema.
-    gdelt_df_mentions = sparkjob_batch.sql_read(sparkjob_batch.bucket_gdelt_old_mentions,
+    # 2. Read historical gdelt mentions data sets, attach schema and combine them.
+    gdelt_df_mentions_old = sparkjob_batch.sql_read(sparkjob_batch.bucket_gdelt_old_mentions,
                                                 gdelt_schemas.schemaMentionsGdelt)
+    gdelt_df_mentions_new = sparkjob_batch.sql_read(sparkjob_batch.bucket_mentions_2019_onwards,
+                                                gdelt_schemas.schemaMentionsGdelt)
+    gdelt_combined_mentions_df = sparkjob_batch.combine_dfs(gdelt_df_mentions_old, gdelt_df_mentions_new)
 
     # 3. Filter GDELT events and mentions data
-    filtered_gdelt_v2_df = sparkjob_batch.filter_gdelt_events(gdelt_df_events)
-    filtered_mentions_df = sparkjob_batch.filter_gdelt_mentions(gdelt_df_mentions)
+    filtered_gdelt_events_df = sparkjob_batch.filter_gdelt_events(gdelt_combined_events_df)
+    filtered_mentions_df = sparkjob_batch.filter_gdelt_mentions(gdelt_combined_mentions_df)
 
     # 4. Join both dataset on common column: Globaleventid
     joined_df = sparkjob_batch.join_df(filtered_gdelt_v2_df, filtered_mentions_df)

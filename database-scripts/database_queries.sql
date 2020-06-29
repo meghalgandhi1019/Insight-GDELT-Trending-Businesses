11592cf63e7865a2ca1f18372a263b5d361bfe5f
Meghal Gandhi <meghalgandhi@Meghals-MBP.lan1>
2020-06-29 00:06:53 -0700

    Added PostgreSQL database queries file

---
+++ database-scripts/database_queries.sql
@@ -0,0 +1,7 @@
+/* PostgreSQL: create database named gdeltdatabase */
+CREATE DATABASE gdeltdatabase;
+
+/* PostgreSQL: create table named gdeltdetails */
+CREATE TABLE gdeltdetails (event_counts integer, state char(4), monthyear date, year integer, actor1type1code char(3), GoldsteinScale_norm_sum Double precision, confidence_sum Double precision);
+
+
